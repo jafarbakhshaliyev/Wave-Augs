0f3d4d99459bca510ad8a1a79f3c114cbae682fd
Jafar Bakhshaliyev <88124269+jafarbakhshaliyev@users.noreply.github.com>
2024-04-18 20:19:59 +0200

    Update DLinear.py

--- models/DLinear.py
+++ models/DLinear.py
@@ -63,7 +63,7 @@ class Model(nn.Module):
             self.Linear_Seasonal = nn.Linear(self.seq_len,self.pred_len)
             self.Linear_Trend = nn.Linear(self.seq_len,self.pred_len)
 
-            # Use this two lines if you want to visualize the weights
+            # Use this two lines if you want to visualize weights
             # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
             # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
 
